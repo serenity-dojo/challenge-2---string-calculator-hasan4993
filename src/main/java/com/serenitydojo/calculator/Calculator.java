3349fc2aa9602f45abcb6fad46fd66b7e20d6ffa
Ali Ul Hasan <syedali0336@gmail.com>
2022-11-13 20:39:52 +0500

    String Calculator Completed - Ali Ul Hasan

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,49 @@
 package com.serenitydojo.calculator;
 
+import com.google.common.base.Splitter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
 public class Calculator {
-    public int evaluate(String expression) {
-        return 0;
+    public int evaluate(@NotNull String expression) {
+        if (expression.isEmpty()) {
+            return 0;
+        }
+        /*Splitter Class provides various methods to handle splitting operations on string, objects
+         * SplitsToList: sequence into string components and returns them as an immutable list.
+         *  E.g: "4+5" it will Split it into "4" "+" "5"
+         */
+        List<String> signs;
+        signs = Splitter.on(" ").splitToList(expression);
+        int currentTotal = 0;
+        String newOperator = "+";
+// Here for Loop will be looking for the Sign if the Sign is gotten it will assign a new sign to the NewOperator
+        for (String sign : signs) {
+            if (!isNumeric(sign)) {
+                newOperator = sign;
+            } else {
+                currentTotal = calculation(currentTotal, newOperator, sign);
+            }
+        }
+        return currentTotal;
+
+    }
+
+    private int calculation(int currentTotal, @NotNull String newOperator, String sign) {
+        switch (newOperator) {
+            case "+":
+                return currentTotal + Integer.parseInt(sign);
+            case "-":
+                return currentTotal - Integer.parseInt(sign);
+            case "*":
+                return currentTotal * Integer.parseInt(sign);
+            case "/":
+                return currentTotal / Integer.parseInt(sign);
+            default:
+                throw new ArithmeticException("Operator is Not Valid " + newOperator);
+        }
     }
 }
